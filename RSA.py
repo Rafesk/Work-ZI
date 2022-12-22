4c0e4812aa74c2e66ae9e6152c40a773b3bb64ca
Ksenia Balalaeva <balalaeva.ks@mail.ru>
2022-12-22 12:27:29 +0300

    Oztatochn File

--- RSA.py
+++ RSA.py
@@ -1,16 +1,19 @@
-from modular_arithmetic import gcd_extended,mulinv
+from modular_arithmetic import mulinv
 
-def RSA(p,q,text):
-    n = p*q
-    yn = (p-1)*(q-1)
-    e = 3
-    d = mulinv(e,yn)
-    C = text**(e) % n
-    Mc = C**d % n
-    print("C=",C)
-    print("M=",Mc)
-
-p = 1787 #2339 
-q = 2039 #1709 
-text = 926526 #1142599 
-RSA(p,q,text)
+p = 1787
+q = 2039  
+text = 926526
+n = p*q
+yn = (p-1)*(q-1)
+e = 3
+d = mulinv(e,yn)
+C = text**(e) % n
+Mc = C**d % n
+print("C=",C)
+print("M=",Mc)
+st = d %(p-1)
+r1 = ((C**p) **st)% p
+st = d % (q-1)
+r2 = ((C%q)**st) % q
+print(r1)
+print(r2)
